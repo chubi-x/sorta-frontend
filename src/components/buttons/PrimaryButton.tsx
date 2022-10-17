e4875ce86588b2932dac89511aead2a9ac5e448e
chubi-x <chubixax@protonmail.com>
2022-10-17 23:41:12 +0100

    remove unnecessary use effect

---
+++ src/components/buttons/PrimaryButton.tsx
@@ -0,0 +1,7 @@
+export function PrimaryButton() {
+  return (
+    <button className="primary__btn primary__btn__medium">
+      <a href={"http://localhost:3000"}>Connect Twitter</a>
+    </button>
+  );
+}
