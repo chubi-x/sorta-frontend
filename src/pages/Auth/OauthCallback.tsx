9f293f6f340973ef615cb6e5a3a07f18d2b0e6a5
chubi-x <chubixax@protonmail.com>
2022-10-25 14:56:58 +0100

    remove unnecessary imports

--- src/pages/Auth/OauthCallback.tsx
+++ src/pages/Auth/OauthCallback.tsx
@@ -1,15 +1,10 @@
-import { useEffect, useContext } from "react";
 import { parse } from "qs";
 import { useLocation } from "react-router-dom";
-import { LoginContext } from "../../helpers/Context";
-import { dom } from "@fortawesome/fontawesome-svg-core";
 
 export function OauthCallback() {
   const params = useLocation();
   const queryString = parse(params.search, { ignoreQueryPrefix: true });
-
   localStorage.setItem("callbackParams", JSON.stringify(queryString));
-  // console.log(window.opener);
   self.close();
   return <h1>Login successful! You can close this window.</h1>;
 }
