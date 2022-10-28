5351999f6df47d1afc7f4b0829a0ab2f93ebadba
chubi-x <chubixax@protonmail.com>
2022-10-28 02:50:15 +0100

    chore: proper bem methodology

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -2,13 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 export function PrimaryButton({ authFunction }: { authFunction: any }) {
   return (
-    <button
-      className="primary__btn primary__btn__medium"
-      onClick={authFunction}
-    >
-      {/* <a href=""> */}
+    <button className="primary-btn primary-btn--medium" onClick={authFunction}>
       Connect Twitter <FontAwesomeIcon icon={faTwitter} />
-      {/* </a> */}
     </button>
   );
 }
