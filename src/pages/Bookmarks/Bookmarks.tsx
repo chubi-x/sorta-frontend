f1c97dae3d6669aa0a8676828a9f46d6b3927204
chubi-x <chubixax@protonmail.com>
2022-10-29 23:36:46 +0100

    feat: add bookmarks subtext

--- src/pages/Bookmarks/Bookmarks.tsx
+++ src/pages/Bookmarks/Bookmarks.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { fetchBookmarks } from "../../api/bookmarks-api";
 import { NewCategoryButton } from "../../components/buttons";
 import { Bookmark } from "./Bookmark";
+import help from "../../assets/icons/help.svg";
 export function Bookmarks() {
   const [bookmarks, setBookmarks] = useState<Bookmarks>();
 
@@ -26,7 +27,13 @@ export function Bookmarks() {
       <NewCategoryButton />
 
       <div className="bookmarks">
-        <div className="mt-10">
+        <div className="flex justify-between text-primary-1">
+          <p className="font-semibold">All Tweets</p>
+          <p className="flex cursor-pointer space-x-2 font-medium">
+            <img src={help} alt="help icon" /> <span>Need Help?</span>
+          </p>
+        </div>
+        <div className="mt-6">
           {bookmarks?.data.map((bookmark: Bookmark) =>
             bookmark?.status === "fulfilled" ? (
               <Bookmark bookmark={bookmark} key={bookmark.value.id} />
