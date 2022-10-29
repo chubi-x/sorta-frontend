8ff31fcc03fa420aa9cac630115c9e19c60bfbce
chubi-x <chubixax@protonmail.com>
2022-10-29 19:41:04 +0100

    feat: refactor oauth flow

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,6 +1,6 @@
-import { fetchOauth } from "../../api";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { LoginContext } from "../../helpers/Context";
+import { fetchOauth } from "../../api";
 
 import Lottie from "lottie-react";
 import login from "../../assets/lotties/login.json";
@@ -9,41 +9,31 @@ import { Navbar } from "../../layouts/Navbar";
 import { PrimaryButton } from "../../components/buttons";
 
 export function Login() {
-  const { setLoading, callback, oauth } = useContext(LoginContext);
+  const { oauth } = useContext(LoginContext);
 
   function authFunction() {
     window.open(oauth.oauthData?.url, "", "width=700;height=700")!;
   }
 
-  function readCallbackMessage(event: StorageEvent) {
-    const callbackParams: CallbackQueryParams = JSON.parse(
-      localStorage.getItem("callbackParams")!
-    );
-    callback.setCallbackParams(callbackParams);
-    localStorage.removeItem("callbackParams");
-  }
   // Begin oauth process
   useEffect(() => {
     const abortController = new AbortController();
-    fetchOauth(abortController, oauth.setOauthData);
-    window.addEventListener("storage", readCallbackMessage);
+    const fetchOauthFunction = async () => {
+      const responseData = await fetchOauth(abortController);
+      console.log(responseData);
+      if (responseData?.success) {
+        console.log("success");
+        oauth.setOauthData(() => responseData?.data);
+      } else {
+        alert("error fetching oauth");
+        console.log(responseData);
+      }
+    };
+    fetchOauthFunction();
     return () => {
       abortController.abort();
-      window.removeEventListener("storage", readCallbackMessage);
     };
   }, []);
-
-  // effect to complete sign in process
-  useEffect(() => {
-    if (callback.callbackParams?.state) {
-      setLoading(true);
-    }
-    if (callback.callbackParams?.error) {
-      alert(callback.callbackParams?.error);
-    }
-
-    return () => {};
-  }, [callback.callbackParams]);
   return (
     <>
       <Navbar authFunction={authFunction} />
