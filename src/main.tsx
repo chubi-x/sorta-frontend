7021fcbc0bcf70b108327d1eceb86059d983c184
Chubiyojo Adejoh <chubixax@protonmail.com>
2022-11-30 12:07:20 +0100

    refactor: wrap entire app component in query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./assets/styles/index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+
   // </React.StrictMode>
 );
-
