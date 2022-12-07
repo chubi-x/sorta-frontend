5b52f157c27d7eb264ccfe73df15b47d0f08ada7
Chubiyojo Adejoh <chubixax@protonmail.com>
2022-12-07 15:15:16 +0100

    chore: reorder state variables

--- src/pages/Bookmarks/Bookmarks.tsx
+++ src/pages/Bookmarks/Bookmarks.tsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 // APIS
 import { fetchBookmarks } from "../../api/bookmarks-api";
+import { useQuery } from "react-query";
+
 // COMPONENTS
 import { Bookmark } from "./Bookmark";
 import withAddBookmarkToCategory from "../../helpers/hocs/withAddBookmarkToCategory";
@@ -12,27 +14,20 @@ import { DashboardBarText } from "../../components/miscellaneous";
 import { Spinner } from "../../assets/animations";
 
 // TYPES
-import { useInView } from "react-intersection-observer";
-import { useQuery } from "react-query";
-
 type BookmarksProps = {
   userFetched: boolean;
   bookmarksScrollRef: React.RefObject<HTMLDivElement>;
 };
 const Bookmarks = memo(({ userFetched, bookmarksScrollRef }: BookmarksProps) => {
-  const [inViewRef, inView] = useInView({
-    root: document.querySelector(".dashboard"),
-    initialInView: true,
-    threshold: 1,
-    rootMargin: "175px",
-    triggerOnce: true,
-  });
   const navigate = useNavigate();
   const [bookmarksLoading, setBookmarksLoading] = useState(false);
 
   const [bookmarks, setBookmarks] = useState<Bookmarks>(
     JSON.parse(sessionStorage.getItem("bookmarks")!) || null
   );
+  const bookmarksPerPage = 20;
+  const [hasMoreBookmarks, setHasMoreBookmarks] = useState(true);
+  const [numOfBookmarksToRender, setNumOfBookmarksToRender] = useState(bookmarksPerPage);
 
   useQuery("fetch-bookmarks", fetchBookmarks, {
     enabled: userFetched,
@@ -49,15 +44,9 @@ const Bookmarks = memo(({ userFetched, bookmarksScrollRef }: BookmarksProps) => 
     },
     onError(err) {
       if (err) alert(err);
-
       navigate("/login");
     },
   });
-
-  console.log("bookmarks rendered");
-  const bookmarksPerPage = 20;
-  const [hasMoreBookmarks, setHasMoreBookmarks] = useState(true);
-  const [numOfBookmarksToRender, setNumOfBookmarksToRender] = useState(bookmarksPerPage);
 
   const renderBookmarks = (bookmarksArray: Bookmark[] | undefined): JSX.Element[] => {
     let renderedBookmarks = [];
@@ -117,12 +106,10 @@ const Bookmarks = memo(({ userFetched, bookmarksScrollRef }: BookmarksProps) => 
 
   return (
     <>
-      <div className={`bookmarks__wrapper ${!inView ? "bookmarks__wrapper--scrolled" : ""}`}>
+      <div className="bookmarks__wrapper">
         <div className="bookmarks" ref={bookmarksScrollRef}>
           <DashboardBarText>
-            <p className="font-semibold " ref={inViewRef}>
-              {bookmarks ? bookmarks?.data.length : ""} Tweets
-            </p>
+            <p className="font-semibold ">{bookmarks ? bookmarks?.data.length : ""} Tweets</p>
           </DashboardBarText>
 
           <InfiniteScroll
