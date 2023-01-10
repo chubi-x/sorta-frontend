2161cab0b2ead146211d8964ed646d7eddde2da0
Chubiyojo Adejoh <chubixax@protonmail.com>
2023-01-10 15:46:18 +0100

    chore: remove unused import

--- src/pages/Categories/Category.tsx
+++ src/pages/Categories/Category.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { BookmarksContextInterface } from "../../App";
 
 import { Menu } from "../../layouts";
 import { Bookmark } from "../Bookmarks";
