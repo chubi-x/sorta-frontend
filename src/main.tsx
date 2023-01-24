e7f6cea192f06fcc67cff97e04beac30abc40a8a
chubi-x <chubixax@protonmail.com>
2023-01-24 15:09:25 +0000

    feat: implement logging

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -11,6 +13,15 @@ import "./assets/styles/index.css";
 
 const queryClient = new QueryClient();
 
+Sentry.init({
+  dsn: "https://fc23a7ff68e343558c88f82b43b0df81@o4504559962882048.ingest.sentry.io/4504559967010817",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
